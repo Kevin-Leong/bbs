f38d8fd2c8068c2d5876217174f8dbc11727fa12
Kevin-Leong <kevin.leong.cn@gmail.com>
2015-06-26 11:50:07 +0800

    navbar and footer

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150624085150) do
+ActiveRecord::Schema.define(version: 20150626033718) do
 
-  create_table "topics", force: :cascade do |t|
+  create_table "groups", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
