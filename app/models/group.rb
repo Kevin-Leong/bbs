4e0b0e7b3b49600a71a007af200f8e0338303ca8
Kevin-Leong <kevin.leong.cn@gmail.com>
2015-06-27 10:36:21 +0800

    user

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,10 @@ class Group < ActiveRecord::Base
   validates :title, presence: {message: '此栏位不可是空白！'}
 
   has_many :posts    
+
+  belongs_to :owner, class_name: "User", foreign_key: :user_id
+
+  def editable_by?(user)
+    user && user == owner
+  end
 end
